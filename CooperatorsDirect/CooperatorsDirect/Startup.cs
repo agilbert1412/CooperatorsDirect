b2eee5c4bd08348552fb53709d525bc1d5d0b1f5
Alex Gilbert <alexgilbert@yahoo.com>
2016-01-26 21:24:45 -0500

    projet original

---
+++ CooperatorsDirect/CooperatorsDirect/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CooperatorsDirect.Startup))]
+namespace CooperatorsDirect
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
