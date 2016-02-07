4b9035254c1b193b39543389f3c5bcac09566d68
agilbert1412 <alexgilbert@yahoo.com>
2016-02-07 15:29:03 -0500

    Connexion et inscription fonctionnent mais sont laids

---
+++ CooperatorsDirect/CooperatorsDirect/Security/CustomAuthorizeAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using CooperatorsDirect.Models;
+
+namespace CooperatorsDirect.Security
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        public Roles[] UserRoles { get; set; }
+
+        public CustomAuthorizeAttribute(params Roles[] userRoles)
+        {
+            UserRoles = userRoles;
+        }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (SessionPersiter.User == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "User", action = "Index" }));
+            }
+            else
+            {
+                UserModel userMod = new UserModel();
+
+                CustomPrincipal customPrinc = new CustomPrincipal(userMod.Find(SessionPersiter.User.Email));
+
+                if (!customPrinc.IsInRole(UserRoles))
+                {
+                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "AccessDenied", action = "Index" }));
+                }
+
+            }
+        }
+    }
+}
---
+++ CooperatorsDirect/CooperatorsDirect/Security/CustomPrincipal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using CooperatorsDirect.Models;
+
+namespace CooperatorsDirect.Models
+{
+    public class CustomPrincipal
+    {
+        private Client User;
+
+        public IIdentity Identity { get; set; }
+
+
+        public CustomPrincipal(Client user)
+        {
+            User = user;
+            Identity = new GenericIdentity(User.Email);
+        }
+
+        public bool IsInRole(Roles[] userRoles)
+        {
+            return userRoles.Any(ur => ur.ToString().Contains(User.Role.ToString()));
+        }
+    }
+}
