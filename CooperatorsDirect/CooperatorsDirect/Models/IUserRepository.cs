610b2811efd49ca6b0e35c667f3204dae1b01be6
agilbert1412 <alexgilbert@yahoo.com>
2016-02-07 14:38:38 -0500

    connexion fonctionne au minimum

---
+++ CooperatorsDirect/CooperatorsDirect/Models/IUserRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CooperatorsDirect.Models
+{
+    public interface IUserRepository
+    {
+
+        Client Find(string email);
+
+        List<Client> GetAll();
+
+        Client Get(string email, string password);
+
+        bool Delete(string email);
+
+        bool Insert(Client user);
+
+        bool Edit(Client user);
+
+    }
+}
